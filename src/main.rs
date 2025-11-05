5cfbb76f8e3ca2ae8da39c9a4b5cb593a8a4d5a9
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-11-05 14:32:44 +0530

    Scaffold the CLI structure using `clap`
    Add a placeholder `snapshot` subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+// Library
+use clap::{Parser, Subcommand};
+
+// ---
+// CLI
+// ---
+
+// Command-Line-Interface
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+// SubCommands
+#[derive(Subcommand)]
+enum Commands {
+    /// Takes a snapshot of a directory
+    Snapshot {
+        /// The path to the directory to snapshot
+        #[arg(value_name = "PATH", default_value = ".")]
+        path: String,
+    },
+}
+
+// ----
+// MAIN
+// ----
+
+// The main entrypoint of the application
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Snapshot { path } => {
+            println!("Snapshot Target: {}", path);
+        }
+    }
 }
